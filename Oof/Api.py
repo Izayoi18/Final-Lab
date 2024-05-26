f39518d6e5a1a347c3428b46019f34b1388e9c00
Izayoi18 <joshuaakamatsu18@gmail.com>
2024-05-26 16:55:02 +0800

    Updated the Api file to connect to Mysql db

--- Oof/Api.py
+++ Oof/Api.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "Akamatsu"
+app.config["MYSQL_DB"] = "vrapmsupdated"
 
-@app.route('/')
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+mysql = MySQL(app)
+
+
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return "<p>Hello, World!</p>"
 
-if __name__ == '__main__':
+@app.route("/drivers", methods=["GET"])
+def get_drivers():
+    cur = mysql.connection.cursor()
+    query="""
+    select * from driver
+    """
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+
+    return make_response(jsonify(data), 200)
+
+if __name__ == "__main__":
     app.run(debug=True)
