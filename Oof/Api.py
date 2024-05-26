c1113a1f57e265f09f7afac5835e7c83b4ae26f4
Izayoi18 <joshuaakamatsu18@gmail.com>
2024-05-26 20:45:02 +0800

    Updating api and test.py to be able to filter data

--- Oof/Api.py
+++ Oof/Api.py
@@ -16,27 +16,28 @@ mysql = MySQL(app)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/license", methods=["GET"])
-def get_drivers():
+@app.route("/licenses", methods=["GET"])
+def get_licenses():
     cur = mysql.connection.cursor()
-    query="""
+    query = """
     select * from license
     """
     cur.execute(query)
     data = cur.fetchall()
     cur.close()
-
     return make_response(jsonify(data), 200)
-"""""@app.route("/license/<int:id>", method=["GET"])
-def get_driver_by_name(name):
+
+@app.route("/licenses/<int:id>", methods=["GET"])
+def get_license_by_id(id):
     cur = mysql.connection.cursor()
-    query = """"select * from license""""
+    query = """
+        select * from license where id = {}
+        """.format(id)
     cur.execute(query)
     data = cur.fetchall()
     cur.close()
+    return make_response(jsonify(data), 200)
 
-
-"""
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Oof/test.py
+++ Oof/test.py
@@ -14,9 +14,15 @@ class MyAppTest(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data.decode(), "<p>Hello, World!</p>")
 
-    def test_getdrivers(self):
-        response = self.app.get("/license")
+    def test_getlicense(self):
+        response = self.app.get("/licenses")
         self.assertEqual(response.status_code, 200)
         self.assertTrue("John Doe" in response.data.decode())
+
+    def test_getlicense_by_id(self):
+        response = self.app.get("/licenses/4")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("Bob Brown" in response.data.decode())
+
 if __name__ == "__main__":
     unittest.main()
