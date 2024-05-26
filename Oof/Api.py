8d13a5c1c03b563c6d8e93c6fffa34400b6f20d4
Izayoi18 <joshuaakamatsu18@gmail.com>
2024-05-27 00:36:40 +0800

    updatng Api.py by inserting scripts to further improve search for data

--- Oof/Api.py
+++ Oof/Api.py
@@ -16,28 +16,32 @@ mysql = MySQL(app)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/licenses", methods=["GET"])
-def get_licenses():
+def data_fetch(query):
     cur = mysql.connection.cursor()
-    query = """
-    select * from license
-    """
     cur.execute(query)
     data = cur.fetchall()
     cur.close()
+    return data
+
+@app.route("/licenses", methods=["GET"])
+def get_licenses():
+    data = data_fetch("""select * from license""")
     return make_response(jsonify(data), 200)
 
 @app.route("/licenses/<int:id>", methods=["GET"])
 def get_license_by_id(id):
-    cur = mysql.connection.cursor()
-    query = """
-        select * from license where id = {}
-        """.format(id)
-    cur.execute(query)
-    data = cur.fetchall()
-    cur.close()
+    data = data_fetch("""select * from license where id = {}""".format(id))
     return make_response(jsonify(data), 200)
 
-
+@app.route("/licenses/<int:id>/violation_description", methods=["GET"])
+def get_violation_description_by_license(id):
+    data = data_fetch("""
+        SELECT license.name, violations.violation_description, violations.penalty, vehicle.vehicle_color
+        FROM license
+        JOIN violations ON license.id = violations.license_id
+        JOIN vehicle ON license.id = vehicle.license_id
+        WHERE license.id = 5;
+    """.format(id))
+    return make_response(jsonify({"license_id" : id, "count": len(data), "violation_description": data}),201)
 if __name__ == "__main__":
     app.run(debug=True)
